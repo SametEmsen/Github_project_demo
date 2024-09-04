062d338ba01814b42d4bf3db0f08db3b323cc2c5
SametEmsen <sametemsen@gmail.com>
2024-09-04 14:46:37 +0300

    samet v4 added and samet v1 updated

--- src/main/java/com/github/samet01.java
+++ src/main/java/com/github/samet01.java
@@ -1,9 +1,7 @@
 package com.github;
 
 public class samet01 {
-    // first commit
-    // second commit
-    // third commit
+
     //      buraya yeni girdi    yepyeni
     // fifth commit
     // sixth commit
---
+++ src/main/java/com/github/samet04.java
@@ -0,0 +1,5 @@
+package com.github;
+
+public class samet04 {
+    // burada yeni bir sayfa çalışması yaotık
+}
