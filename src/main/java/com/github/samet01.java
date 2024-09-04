f3d4fc5003b58f006b95083abc92311a8910dcb1
SametEmsen <146114142+SametEmsen@users.noreply.github.com>
2024-09-04 14:48:10 +0300

    Update samet01.java

--- src/main/java/com/github/samet01.java
+++ src/main/java/com/github/samet01.java
@@ -4,7 +4,7 @@ public class samet01 {
     // first commit
     // second commit
     // third commit
-    //      buraya yeni girdi    yepyeni    daha da yeni
+    //      buraya yeni girdi     daha da yeni
     // fifth commit
     // sixth commit
     // seventh commit
